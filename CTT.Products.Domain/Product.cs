3ee4eaa8259b4751f125009dd657da3e3634fa5a
Diego Pereira <diego.pereira.externo@ageas.pt>
2025-04-20 22:57:14 +0100

    -Solution setup -Tests created -Product domain created -Tests improved

---
+++ CTT.Products.Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Products.Domain;
+
+public record Product
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string[] Categories { get; set; } = [];
+}
